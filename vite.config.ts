163f1c7ee991dc77f2d236de8442d204a634aa47
Алексей Андрюшин <aleksroebuck@gmail.com>
2025-10-17 17:27:14 +0300

    **

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     }
   },
-  define: {
-    'import.meta.env.VITE_TMDB_AUTH_KEY': JSON.stringify(VERCEL_AUTH_KEY),
-  },
+  // define: {
+  //   'import.meta.env.VITE_TMDB_AUTH_KEY': JSON.stringify(VERCEL_AUTH_KEY),
+  // },
 })
