ecab0836a31bc6b7bdf2a6290d98ca35cd526cbb
Алексей Андрюшин <aleksroebuck@gmail.com>
2025-10-17 17:29:41 +0300

    **

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-const VERCEL_AUTH_KEY = process.env.TMDB_AUTH_KEY || process.env.TMDB_API_KEY_V3;
+const VERCEL_AUTH_KEY = process.env.TMDB_API_ACCESS_TOKEN || process.env.TMDB_API_KEY_V3;
 
 export default defineConfig({
   plugins: [react()],
@@ -11,7 +11,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     }
   },
-  // define: {
-  //   'import.meta.env.VITE_TMDB_AUTH_KEY': JSON.stringify(VERCEL_AUTH_KEY),
-  // },
+  define: {
+    'import.meta.env.VITE_TMDB_AUTH_KEY': JSON.stringify(VERCEL_AUTH_KEY),
+  },
 })
